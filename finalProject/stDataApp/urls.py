b7d2b30fd894e3f961a3e7f0e00273a0a21be479
schamberi <schamberi@hotmail.com>
2024-02-21 10:56:33 +0100

    changed api

--- finalProject/stDataApp/urls.py
+++ finalProject/stDataApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     path('uploadexcel/', TryStoringExcel.as_view(), name='stat-upload'), #prov ok po sduhet ishte prove
     path('download-excel/<int:file_id>/', DownloadRawData.as_view(), name='download_excel'), #kjo duhet sepse i ben automatikisht download filet ne browser
-    path('upload-data/', StandartDataMapping.as_view(), name='standard-upload'),
+    path('upload-data/',StandartDataMapping.as_view(), name='standard-upload'),
 ]
 
 
